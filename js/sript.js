d8d5d03666d798c10d2116dfa4728f1cef778838
Dinis <isakovdr@gmail.com>
2022-11-13 15:44:23 +0300

    first

---
+++ js/sript.js
@@ -0,0 +1,15 @@
+// alert('Hello')
+
+// const result = confirm('Yes?');
+
+// console.log(result);
+
+const answer = [];
+
+answer[0] = prompt('как дела?', '');
+answer[1] = prompt('как зовут?', '');
+answer[2] = prompt('сколько лет?', '');
+
+console.log(answer);
+
+document.write(answer);
